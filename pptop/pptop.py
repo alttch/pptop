198d97aab9ce018d61135854cf2ce49e257f3dcb
divisor <div@altertech.com>
2019-08-08 23:59:05 +0200

    pager and bottom bar

--- pptop/pptop.py
+++ pptop/pptop.py
@@ -25,20 +25,21 @@ work_pid = None
 
 
 def select_process(stdscr):
-    stdscr.clear()
-    curses.curs_set(0)
-    if curses.can_change_color():
-        curses.init_color(0, 0, 0, 0)
-    height, width = stdscr.getmaxyx()
-    stdscr.addstr(0, 0, 'Select process', curses.color_pair(5) | curses.A_BOLD)
-    stdscr.addstr(1, 0, '-' * width, curses.color_pair(9))
-    index = 0
     processes = []
     for p in psutil.process_iter():
         name = p.name()
         if name in ['python', 'python3'] and p.pid != os.getpid():
             processes.append(p)
-
+    if not processes:
+        raise Exception('No processed found')
+    stdscr.clear()
+    curses.curs_set(0)
+    if curses.can_change_color():
+        curses.init_color(0, 0, 0, 0)
+    height, width = stdscr.getmaxyx()
+    stdscr.addstr(0, 0, 'Select process', curses.color_pair(4) | curses.A_BOLD)
+    stdscr.addstr(1, 0, '-' * width, curses.color_pair(9))
+    index = 0
     while True:
         i = 0
         for p in processes:
@@ -80,7 +81,8 @@ def show_process_info(stdscr, p, **kwargs):
             ct = p.cpu_times()
             stdscr.move(0, 0)
             stdscr.addstr('Process: ')
-            stdscr.addstr(' '.join(p.cmdline()), curses.color_pair(4))
+            cmdline = ' '.join(p.cmdline())[:width - 20]
+            stdscr.addstr(cmdline, curses.color_pair(4))
             stdscr.addstr(' [')
             stdscr.addstr(str(p.pid), curses.color_pair(3) | curses.A_BOLD)
             stdscr.addstr(']')
@@ -107,24 +109,89 @@ def show_process_info(stdscr, p, **kwargs):
 def print_section_title(stdscr, title):
     height, width = stdscr.getmaxyx()
     stdscr.addstr(3, 0, ' ' + title.ljust(width - 1),
-                  curses.color_pair(3) | curses.A_REVERSE)
+                  curses.color_pair(4) | curses.A_BOLD)
 
 
-def fancy_tabulate(stdscr, table):
+def fancy_tabulate(stdscr, table, cursor=None):
     if table:
+        height, width = stdscr.getmaxyx()
         d = tabulate.tabulate(table, headers='keys').split('\n')
-        stdscr.addstr(4, 0, d[0], curses.color_pair(5))
-        stdscr.addstr(5, 0, d[1], curses.color_pair(9))
-        stdscr.addstr(6, 0, '\n'.join(d[2:]))
+        stdscr.addstr(4, 0, d[0].ljust(width),
+                      curses.color_pair(3) | curses.A_REVERSE)
+        for i, t in enumerate(d[2:]):
+            stdscr.addstr(
+                5 + i, 0, t.ljust(width),
+                curses.color_pair(7) | curses.A_REVERSE
+                if cursor == i else curses.A_NORMAL)
+
+
+def print_bottom_bar(stdscr):
+    hlp = {'F5': 'Prof', 'F6': 'Files', 'F7': 'Thrds'}
+    height, width = stdscr.getmaxyx()
+    stdscr.move(height - 1, 0)
+    stdscr.addstr(' ' * (width - 1), curses.color_pair(7) | curses.A_REVERSE)
+    stdscr.move(height - 1, 0)
+    for h, t in hlp.items():
+        stdscr.addstr(h)
+        stdscr.addstr(t.ljust(6), curses.color_pair(7) | curses.A_REVERSE)
+    return
 
 
 @atasker.background_worker(delay=1)
 def show_function_profiler(stdscr, p, **kwargs):
     height, width = stdscr.getmaxyx()
     with scr_lock:
-        print_section_title(stdscr, 'Profiler')
+        print_section_title(stdscr, 'Function profiler')
         stdscr.clrtobot()
+        print_bottom_bar(stdscr)
         stdscr.refresh()
+
+
+reserved_lines = 6
+
+
+def handle_pager_event(stdscr, cursor_id, max_pos):
+    height, width = stdscr.getmaxyx()
+    crs = getattr(_d, cursor_id + '_cursor')
+    shf = getattr(_d, cursor_id + '_shift')
+    if _d.key_event:
+        if _d.key_event == 'KEY_DOWN':
+            crs += 1
+            if crs > max_pos:
+                crs = max_pos
+            if crs - shf >= height - reserved_lines:
+                shf += 1
+        elif _d.key_event == 'KEY_UP':
+            crs -= 1
+        elif _d.key_event == 'KEY_NPAGE':
+            crs += height - reserved_lines
+            shf += height - reserved_lines
+        elif _d.key_event == 'KEY_PPAGE':
+            crs -= height - reserved_lines
+            shf -= height - reserved_lines
+        elif _d.key_event == 'KEY_HOME':
+            crs = 0
+            shf = 0
+        elif _d.key_event == 'KEY_END':
+            crs = max_pos
+            shf = max_pos - height + reserved_lines + 1
+        if crs < 0:
+            shf -= 1
+            crs = 0
+        if crs - shf < 0:
+            crs = shf - 1
+            shf -= 1
+        if shf < 0:
+            shf = 0
+    if max_pos <= height - reserved_lines + 1:
+        shf = 0
+    if crs > max_pos:
+        crs = max_pos
+        shf = max_pos - height + reserved_lines + 1
+        if shf < 0:
+            shf = 0
+    setattr(_d, cursor_id + '_cursor', crs)
+    setattr(_d, cursor_id + '_shift', shf)
 
 
 @atasker.background_worker(delay=1)
@@ -138,15 +205,25 @@ def show_open_files(stdscr, p, **kwargs):
             'pos.': f.position,
             'mode': f.mode
         })
+    files = sorted(files, key=lambda k: k['path'])
     with scr_lock:
-        print_section_title(stdscr, 'Files')
-        files = sorted(files, key=lambda k: k['path'])
-        fancy_tabulate(stdscr, files)
+        print_section_title(stdscr, 'Open files')
+        handle_pager_event(stdscr, 'files', len(files) - 1)
+        if _d.key_event:
+            _d.key_event = None
+        fancy_tabulate(
+            stdscr,
+            files[_d.files_shift:_d.files_shift + height - reserved_lines],
+            cursor=_d.files_cursor - _d.files_shift)
         stdscr.clrtobot()
+        print_bottom_bar(stdscr)
         stdscr.refresh()
 
 
-_d = SimpleNamespace(current_worker=None)
+_d = SimpleNamespace(
+    current_worker=None, files_cursor=0, files_shift=0, key_event=None)
+
+default_page = show_open_files
 
 
 def main(stdscr):
@@ -170,9 +247,12 @@ def main(stdscr):
     height, width = stdscr.getmaxyx()
     stdscr.clear()
     curses.curs_set(0)
-    switch_worker(show_function_profiler)
+    switch_worker(default_page)
     atasker.background_task(show_process_info.start)(stdscr=stdscr, p=p)
-    worker_shortcuts = {'f': show_open_files, 'p': show_function_profiler}
+    worker_shortcuts = {
+        'KEY_F(5)': show_function_profiler,
+        'KEY_F(6)': show_open_files
+    }
     while True:
         try:
             k = stdscr.getkey()
@@ -183,8 +263,9 @@ def main(stdscr):
             elif k in worker_shortcuts:
                 switch_worker(worker_shortcuts[k])
             else:
-                show_process_info.trigger()
-                _d.current_worker.trigger()
+                with scr_lock:
+                    _d.key_event = k
+                    _d.current_worker.trigger()
         except:
             raise
             return
@@ -192,5 +273,8 @@ def main(stdscr):
 
 atasker.task_supervisor.start()
 # work_pid = 114958
-curses.wrapper(main)
+try:
+    curses.wrapper(main)
+except Exception as e:
+    print(e)
 atasker.task_supervisor.stop(wait=False)
