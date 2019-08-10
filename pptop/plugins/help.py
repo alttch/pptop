3747185fd17c3cdef1d15c87c335c2c71216b774
divisor <div@altertech.com>
2019-08-10 21:57:14 +0200

    helpmod

---
+++ pptop/plugins/help.py
@@ -0,0 +1,26 @@
+from pptop import GenericPlugin, format_mod_name
+import pptop.core as core
+import textwrap
+
+
+class Plugin(GenericPlugin):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(interval=3600)
+
+    def on_load(self):
+        self.short_name = 'Help'
+        self.sorting_enabled = False
+        self.cursor_enabled = False
+
+    def load_data(self):
+        self.data.clear()
+        self.data.append({'help': ''})
+        pdoc = textwrap.dedent(self._previous_plugin['p'].__doc__)
+        if pdoc:
+            for x in pdoc.strip().split('\n'):
+                self.data.append({'help': x})
+            self.data.append({'help': ''})
+            self.data.append({'help': '-' * self.stdscr.getmaxyx()[1]})
+        for x in textwrap.dedent(core.__doc__).split('\n'):
+            self.data.append({'help': x})
