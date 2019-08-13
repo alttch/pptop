f31a25865431ac5e4b5619644bf47b60ed1ef42c
divisor <div@altertech.com>
2019-08-13 03:25:19 +0200

    script runner

---
+++ pptop/plugins/script_runner.py
@@ -0,0 +1,65 @@
+from pptop import GenericPlugin, palette
+
+import glob
+import os
+
+
+class Plugin(GenericPlugin):
+    '''
+    script_runner plugin: run a custom script
+
+    Executes selected script from ~/.pptop/scripts
+    '''
+
+    def on_load(self):
+        self.title = 'Script runner'
+        self.description = 'Run selected script from ~/.pptop/scripts'
+        self.short_name = 'Script'
+        self.sorting_rev = False
+        self.scripts_dir = self.get_config_dir() + '/scripts'
+
+    def load_data(self):
+        self.data.clear()
+        try:
+            for g in glob.glob(self.scripts_dir + '/**/*.py', recursive=True):
+                self.data.append({'script': g[len(self.scripts_dir) + 1:]})
+        except:
+            pass
+
+    def handle_key_event(self, event, dtd):
+        if event == '\n':
+            script = self.get_selected_row()['script']
+            try:
+                fname = self.scripts_dir + '/' + script
+                with open(fname) as fd:
+                    src = fd.read()
+            except:
+                self.print_message('Unable to load {}'.format(fname))
+            try:
+                result = self.injection_command(src=src)
+            except Exception as e:
+                result = e
+            if not isinstance(result, Exception):
+                if result is None:
+                    msg = '{} executed'.format(script)
+                else:
+                    msg = '{}: {}'.format(script, result)
+                self.print_message(msg, color=palette.GREEN)
+            else:
+                self.print_message(
+                    '{}: {}'.format(script, result), color=palette.ERROR)
+
+    async def run(self, *args, **kwargs):
+        super().run(*args, **kwargs)
+
+    def toggle_pause(self):
+        pass
+
+
+def injection(src, **kwargs):
+    d = {}
+    try:
+        exec(src, d)
+    except Exception as e:
+        return e
+    return d.get('out')
