fc7e7b1d8444b2d5ab347cced022e22e6a0deb16
divisor <div@altertech.com>
2019-08-22 03:25:22 +0200

    0.3.17

--- pptop/injection.py
+++ pptop/injection.py
@@ -46,7 +46,7 @@ server receives "bye" command, it immediately terminate itself and loaded
 plugins.
 '''
 
-__injection_version__ = '0.3.16'
+__injection_version__ = '0.3.17'
 
 import threading
 import struct
