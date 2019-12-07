5007686001f1df24db69104fc4a663afdf37578f
Sergei S <div@altertech.com>
2019-12-07 16:56:06 +0000

    0.5.22

--- pptop/plugin.py
+++ pptop/plugin.py
@@ -1,7 +1,7 @@
 __author__ = 'Altertech, https://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2019 Altertech'
 __license__ = 'MIT'
-__version__ = '0.5.21'
+__version__ = '0.5.22'
 
 import curses
 import sys
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '0.5.21'
+__version__ = '0.5.22'
 
 import setuptools
 
