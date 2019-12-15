9bc0edc6337229565fdcda49e90a584f04ded98e
Sergei S <div@altertech.com>
2019-12-15 20:29:41 +0000

    0.6.1

--- pptop/injection.py
+++ pptop/injection.py
@@ -47,7 +47,7 @@ server receives "bye" command, it immediately terminate itself and loaded
 plugins.
 '''
 
-__injection_version__ = '0.6.0'
+__injection_version__ = '0.6.1'
 
 import threading
 import struct
