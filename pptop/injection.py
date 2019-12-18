97133a491c8c01737409879b6bac45b5e4d1c64f
Sergei S <div@altertech.com>
2019-12-18 11:33:34 +0000

    0.6.7

--- pptop/injection.py
+++ pptop/injection.py
@@ -47,7 +47,7 @@ server receives "bye" command, it immediately terminate itself and loaded
 plugins.
 '''
 
-__injection_version__ = '0.6.5'
+__injection_version__ = '0.6.7'
 
 import threading
 import struct
--- pptop/plugin.py
+++ pptop/plugin.py
@@ -1,7 +1,7 @@
 __author__ = 'Altertech, https://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2019 Altertech'
 __license__ = 'MIT'
-__version__ = '0.6.5'
+__version__ = '0.6.7'
 
 import curses
 import sys
